d2f00f09d3b12150c5a99f7d6236bad895f2eb7b
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-04-26 13:25:42 +0200

    User Password

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 var name = prompt("Write your name, please.");
 var surname = prompt("Write your surname, please.");
 var favoriteColor = prompt("Write your favorite color, please.");
+var number = "21"
 
+console.log(name);
+console.log(surname);
+console.log(favoriteColor);
+
+var userPassword = name + surname + favoriteColor + number;
+
+console.log(userPassword);
+
+document.getElementById("user_password").innerHTML = userPassword
