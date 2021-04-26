bf7073cf46693b6eb0ff8f4659b268752f7e1d3a
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-04-26 13:28:07 +0200

    Js Refactoring

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 var name = prompt("Write your name, please.");
 var surname = prompt("Write your surname, please.");
 var favoriteColor = prompt("Write your favorite color, please.");
-var number = "21"
+var number = "21";
 
+/*
 console.log(name);
 console.log(surname);
 console.log(favoriteColor);
 console.log(number);
+*/
 
 var userPassword = name + surname + favoriteColor + number;
 
-console.log(userPassword);
+// console.log(userPassword);
 
-document.getElementById("user_password").innerHTML = userPassword
+document.getElementById("user_password").innerHTML = userPassword;
